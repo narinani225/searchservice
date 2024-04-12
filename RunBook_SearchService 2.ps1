46bf29c23f394cc4aaea669c678127ea150193e6
narinani225 <100519638+narinani225@users.noreply.github.com>
2024-04-12 14:33:08 +0530

    Update RunBook_SearchService 2.ps1

--- RunBook_SearchService 2.ps1
+++ RunBook_SearchService 2.ps1
@@ -1,29 +1,22 @@
-# Install Git using Chocolatey
-Write-Host "Installing Git..."
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install git -y
-
-# Wait for Git installation to complete before proceeding
-Write-Host "Waiting for Git installation to complete..."
-Start-Sleep -Seconds 30  # Adjust sleep duration as needed
 
 $resourceGroupName = "EwsArmDeploy"
 $searchServiceName = "rgworkspacecognitivesearchtest"
-
+ 
 # Get the admin API key
 $adminApiKey = Get-AzSearchAdminKeyPair -ResourceGroupName $resourceGroupName -ServiceName $searchServiceName
-$adminKey = $adminApiKey.primary
+$adminkey = $adminApiKey.primary
 
 # Store the admin API key in Azure Key Vault
 $adminSecret = ConvertTo-SecureString -String $adminKey -AsPlainText -Force
 $secret = Set-AzKeyVaultSecret -VaultName "rg-workspace-keyvault" -Name "search-Service-API-Key" -SecretValue $adminSecret
 
+ 
 $headers = @{
-    'api-key' = $adminKey
+    'api-key' = $adminkey
     'Content-Type' = 'application/json'
     'Accept' = 'application/json'
 }
-
+ 
 # Create index request body
 $indexBody = @{
     "name" = "azure-blob-blogs-index"
@@ -37,10 +30,108 @@ $indexBody = @{
             "facetable" = $false
             "key" = $false
         },
-        # Add other fields here as needed
+        @{
+            "name" = "BlogOverview"
+            "type" = "Edm.String"
+            "searchable" = $true
+            "filterable" = $false
+            "sortable" = $false
+            "facetable" = $false
+            "key" = $false
+        },
+        @{
+            "name" = "Keywords"
+            "type" = "Edm.String"
+            "searchable" = $true
+            "filterable" = $false
+            "sortable" = $false
+            "facetable" = $false
+            "key" = $false
+        },
+        @{
+            "name" = "BlogStatus"
+            "type" = "Edm.String"
+            "searchable" = $true
+            "filterable" = $true
+            "sortable" = $true
+            "facetable" = $true
+            "key" = $false
+        },
+        @{
+            "name" = "EvokeDepartment"
+            "type" = "Edm.String"
+            "searchable" = $true
+            "filterable" = $true
+            "sortable" = $true
+            "facetable" = $true
+            "key" = $false
+        },
+        @{
+            "name" = "URLLink"
+            "type" = "Edm.String"
+            "searchable" = $false
+            "filterable" = $false
+            "sortable" = $false
+            "facetable" = $false
+            "key" = $false
+        },
+        @{
+            "name" = "Date"
+            "type" = "Edm.DateTimeOffset"
+            "searchable" = $false
+            "filterable" = $false
+            "sortable" = $false
+            "facetable" = $false
+            "key" = $false
+        },
+        @{
+            "name" = "WrittenBy"
+            "type" = "Edm.String"
+            "searchable" = $true
+            "filterable" = $true
+            "sortable" = $true
+            "facetable" = $true
+            "key" = $false
+        },
+        @{
+            "name" = "ItemType"
+            "type" = "Edm.String"
+            "searchable" = $true
+            "filterable" = $false
+            "sortable" = $false
+            "facetable" = $false
+            "key" = $false
+        },
+        @{
+            "name" = "Path"
+            "type" = "Edm.String"
+            "searchable" = $true
+            "filterable" = $false
+            "sortable" = $false
+            "facetable" = $false
+            "key" = $false
+        },
+        @{
+            "name" = "AzureSearch_DocumentKey"
+            "type" = "Edm.String"
+            "searchable" = $true
+            "filterable" = $false
+            "sortable" = $false
+            "facetable" = $false
+            "key" = $true
+        },
+        @{
+            "name" = "DataSource"
+            "type" = "Edm.String"
+            "searchable" = $true
+            "filterable" = $true
+            "sortable" = $true
+            "facetable" = $true
+            "key" = $false
+        }
     )
 } | ConvertTo-Json
-
+ 
 $openAiIndexBody = @{
     "name" = "open-ai-index"
     "fields" = @(
@@ -54,18 +145,77 @@ $openAiIndexBody = @{
             "facetable" = $false
             "key" = $false
         },
-        # Add other fields here as needed
+        @{
+            "name" = "filepath"
+            "type" = "Edm.String"
+            "searchable" = $false
+            "filterable" = $false
+            "retrievable" = $true
+            "sortable" = $false
+            "facetable" = $false
+            "key" = $false
+        },
+        @{
+            "name" = "title"
+            "type" = "Edm.String"
+            "searchable" = $true
+            "filterable" = $false
+            "retrievable" = $true
+            "sortable" = $false
+            "facetable" = $false
+            "key" = $false
+        },
+        @{
+            "name" = "url"
+            "type" = "Edm.String"
+            "searchable" = $false
+            "filterable" = $false
+            "retrievable" = $true
+            "sortable" = $false
+            "facetable" = $false
+            "key" = $false
+        },
+        @{
+            "name" = "id"
+            "type" = "Edm.String"
+            "searchable" = $false
+            "filterable" = $true
+            "retrievable" = $true
+            "sortable" = $true
+            "facetable" = $false
+            "key" = $true
+        },
+        @{
+            "name" = "chunk_id"
+            "type" = "Edm.String"
+            "searchable" = $false
+            "filterable" = $false
+            "retrievable" = $true
+            "sortable" = $false
+            "facetable" = $false
+            "key" = $false
+        },
+        @{
+            "name" = "last_updated"
+            "type" = "Edm.String"
+            "searchable" = $false
+            "filterable" = $false
+            "retrievable" = $true
+            "sortable" = $false
+            "facetable" = $false
+            "key" = $false
+        }
     )
 } | ConvertTo-Json
-
-# URL for index creation
+ 
+ 
 $urlIndex = "https://$($searchServiceName).search.windows.net/indexes/azure-blob-blogs-index?api-version=2023-11-01"
 $urlIndex1 = "https://$($searchServiceName).search.windows.net/indexes/open-ai-index?api-version=2023-11-01"
-
+ 
 # Create the index
 Invoke-RestMethod -Uri $urlIndex -Headers $headers -Method Put -Body $indexBody | ConvertTo-Json
 Invoke-RestMethod -Uri $urlIndex1 -Headers $headers -Method Put -Body $openAiIndexBody | ConvertTo-Json
-
+ 
 # Create datasource request body
 $datasourceBody = @{
     "name" = "azure-blob-datasource"
@@ -77,8 +227,8 @@ $datasourceBody = @{
         "name" = "dev-container-blob"
     }
 } | ConvertTo-Json
-
+ 
 $urlDatasource = "https://$($searchServiceName).search.windows.net/datasources/azure-blob-datasource?api-version=2023-11-01"
-
+ 
 # Create the datasource
 Invoke-RestMethod -Uri $urlDatasource -Headers $headers -Method Put -Body $datasourceBody | ConvertTo-Json
