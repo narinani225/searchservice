2976a8055c7ea57f3f62c712e24f441c32b5add0
narinani225 <100519638+narinani225@users.noreply.github.com>
2024-04-16 02:44:34 +0530

    Update RunBook_SearchService 2.ps1

--- RunBook_SearchService 2.ps1
+++ RunBook_SearchService 2.ps1
@@ -1,6 +1,6 @@
 # Connect to Azure account (ensure you're logged in)
  #Connect-AzAccount -Tenant 'e1c5d9ee-a951-451e-8432-642c23d40071' -SubscriptionId '1d012c12-eb7c-4bc4-aba7-d34e5c20bf00'
- az login --tenant "e1c5d9ee-a951-451e-8432-642c23d40071"
+ az login --tenant e1c5d9ee-a951-451e-8432-642c23d40071
   az account set --subscription "R and D Labs"
  # Set the name of your App Service, storage account, and file share
 appServiceName = "rg-workspace-ui-app"
