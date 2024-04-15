1f6cbea2a94e9a645bbbb2b215a4a5495125d14b
narinani225 <100519638+narinani225@users.noreply.github.com>
2024-04-16 02:38:56 +0530

    Update RunBook_SearchService 2.ps1

--- RunBook_SearchService 2.ps1
+++ RunBook_SearchService 2.ps1
@@ -3,17 +3,17 @@
  az login --tenant "e1c5d9ee-a951-451e-8432-642c23d40071"
   az account set --subscription "R and D Labs"
  # Set the name of your App Service, storage account, and file share
-$appServiceName = "rg-workspace-ui-app"
-$storageAccountName = "rgworkspacesg"
+appServiceName = "rg-workspace-ui-app"
+storageAccountName = "rgworkspacesg"
 
-$fileShareName = "mount"
+fileShareName = "mount"
 # Set the destination path in the container
-$containerPath = "/usr/share/nginx/html/assets"
+containerPath = "/usr/share/nginx/html/assets"
 # Get the resource group name and the name of the App Service plan
-$resourceGroupName = "EwsArmDeploy"
-$appServicePlanName = "rg-workspace-serviceplan"
+resourceGroupName = "EwsArmDeploy"
+appServicePlanName = "rg-workspace-serviceplan"
 # Get the storage account key
-$storageAccountKey = "a5f+AK/Q974cBVEW7uRSR6zTLgo8HvkbvMLTtHZifL42M7TglsHLjN2dUhUR4q4F7cz8wzjmxK5r+AStKwmZAA=="
+storageAccountKey = "a5f+AK/Q974cBVEW7uRSR6zTLgo8HvkbvMLTtHZifL42M7TglsHLjN2dUhUR4q4F7cz8wzjmxK5r+AStKwmZAA=="
 # Add the storage account as a mount
 
 az webapp config storage-account add --resource-group $resourceGroupName --name $appServiceName --custom-id $storageAccountName --storage-type AzureFiles --share-name $fileShareName --account-name $storageAccountName --access-key $storageAccountKey --mount-path $containerPath
