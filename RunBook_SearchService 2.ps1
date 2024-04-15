2d3375fdbb378c29c4f10e3f93f702b8a385fb5f
narinani225 <100519638+narinani225@users.noreply.github.com>
2024-04-15 20:59:26 +0530

    Update RunBook_SearchService 2.ps1

--- RunBook_SearchService 2.ps1
+++ RunBook_SearchService 2.ps1
@@ -1,27 +1,15 @@
-# Set variables
-appServiceName="rg-workspace-ui-app"
-storageAccountName="rgworkspacesg"
-fileShareName="mounttest"
-containerPath="/usr/share/nginx/html/assets"
-
-# Get resource group name
-resourceGroupName=$(az webapp show --name $appServiceName --query 'resourceGroup' -o tsv)
-
-# Get storage account key
-storageAccountKey=$(az storage account keys list --resource-group $resourceGroupName --account-name $storageAccountName --query '[0].value' -o tsv)
-
-# Add storage account as mount
-az webapp config storage-account add \
-    --resource-group $resourceGroupName \
-    --name $appServiceName \
-    --custom-id $storageAccountName \
-    --storage-type AzureFiles \
-    --share-name $fileShareName \
-    --account-name $storageAccountName \
-    --access-key $storageAccountKey \
-    --mount-path $containerPath
-
-# Verify storage mount
-az webapp config storage-account list \
-    --resource-group $resourceGroupName \
-    --name $appServiceName
+# Set the name of your App Service, storage account, and file share
+$appServiceName = "rg-workspace-ui-app"
+$storageAccountName = "rgworkspacesg"
+$fileShareName = "mount"
+# Set the destination path in the container
+$containerPath = "/usr/share/nginx/html/assets"
+# Get the resource group name and the name of the App Service plan
+$resourceGroupName = (Get-AzWebApp -Name $appServiceName).ResourceGroup
+$appServicePlanName = (Get-AzWebApp -Name $appServiceName).ServerFarm
+# Get the storage account key
+$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName)[0].Value
+# Add the storage account as a mount
+az webapp config storage-account add --resource-group $resourceGroupName --name $appServiceName --custom-id $storageAccountName --storage-type AzureFiles --share-name $fileShareName --account-name $storageAccountName --access-key $storageAccountKey --mount-path $containerPath
+# Verify the storage is mounted
+az webapp config storage-account list --resource-group $resourceGroupName --name $appServiceName
