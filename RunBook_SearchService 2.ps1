5a82a0b166fcf0943cedfe0a6f91fb0bd8673a9a
narinani225 <100519638+narinani225@users.noreply.github.com>
2024-04-16 01:21:13 +0530

    Update RunBook_SearchService 2.ps1

--- RunBook_SearchService 2.ps1
+++ RunBook_SearchService 2.ps1
@@ -1,15 +1,28 @@
+# Connect to Azure account (ensure you're logged in)
+Connect-AzAccount
+
 # Set the name of your App Service, storage account, and file share
 $appServiceName = "rg-workspace-ui-app"
 $storageAccountName = "rgworkspacesg"
 $fileShareName = "mount"
+
 # Set the destination path in the container
 $containerPath = "/usr/share/nginx/html/assets"
+
 # Get the resource group name and the name of the App Service plan
-$resourceGroupName = (Get-AzWebApp -Name $appServiceName).ResourceGroup
-$appServicePlanName = (Get-AzWebApp -Name $appServiceName).ServerFarm
+$webApp = Get-AzWebApp -Name $appServiceName
+$resourceGroupName = $webApp.ResourceGroup
+$appServicePlanName = $webApp.ServerFarmId
+
 # Get the storage account key
 $storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName)[0].Value
+
 # Add the storage account as a mount
-az webapp config storage-account add --resource-group $resourceGroupName --name $appServiceName --custom-id $storageAccountName --storage-type AzureFiles --share-name $fileShareName --account-name $storageAccountName --access-key $storageAccountKey --mount-path $containerPath
+Invoke-AzCliCommand -ScriptBlock {
+    az webapp config storage-account add --resource-group $using:resourceGroupName --name $using:appServiceName --custom-id $using:storageAccountName --storage-type AzureFiles --share-name $using:fileShareName --account-name $using:storageAccountName --access-key $using:storageAccountKey --mount-path $using:containerPath
+}
+
 # Verify the storage is mounted
-az webapp config storage-account list --resource-group $resourceGroupName --name $appServiceName
+Invoke-AzCliCommand -ScriptBlock {
+    az webapp config storage-account list --resource-group $using:resourceGroupName --name $using:appServiceName
+}
