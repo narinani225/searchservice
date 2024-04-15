5017993a2f8942b937331c6dcf44d441994596ab
narinani225 <100519638+narinani225@users.noreply.github.com>
2024-04-16 02:16:22 +0530

    Update RunBook_SearchService 2.ps1

--- RunBook_SearchService 2.ps1
+++ RunBook_SearchService 2.ps1
@@ -1,5 +1,5 @@
 # Connect to Azure account (ensure you're logged in)
- Connect-AzAccount -Tenant 'e1c5d9ee-a951-451e-8432-642c23d40071' -SubscriptionId '1d012c12-eb7c-4bc4-aba7-d34e5c20bf00'
+ Connect-AzAccount -Tenant '' -SubscriptionId ''
  # Set the name of your App Service, storage account, and file share
 $appServiceName = "rg-workspace-ui-app"
 $storageAccountName = "rgworkspacesg"
