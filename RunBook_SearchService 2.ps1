45306ec67f8422e6220224c9d0fff5bf36788ebf
narinani225 <100519638+narinani225@users.noreply.github.com>
2024-04-16 00:57:20 +0530

    Update RunBook_SearchService 2.ps1

--- RunBook_SearchService 2.ps1
+++ RunBook_SearchService 2.ps1
@@ -1,17 +1,4 @@
-# Set the name of your App Service, storage account, and file share
-$appServiceName = "rg-workspace-ui-app"
-$storageAccountName = "rgworkspacesg"
-$fileShareName = "mount"
-# Set the destination path in the container
-$containerPath = "/usr/share/nginx/html/assets"
-# Get the resource group name and the name of the App Service plan
-$resourceGroupName = (Get-AzWebApp -Name $appServiceName).ResourceGroup
-$appServicePlanName = (Get-AzWebApp -Name $appServiceName).ServerFarm
-# Get the storage account key
-$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName)[0].Value
-# Add the storage account as a mount
-Set-AzWebApp -Name $appServiceName -ResourceGroupName $resourceGroupName -MountEntry $storageAccountName `
-    -AccountName $storageAccountName -ShareName $fileShareName -AccessKey $storageAccountKey -Type AzureFiles `
-    -CustomId $storageAccountName -DestinationPath $containerPath
-# Verify the storage is mounted
-Get-AzWebApp -Name $appServiceName -ResourceGroupName $resourceGroupName | Select-Object -ExpandProperty MountEntries
+$i=12
+$j=43
+$k=$i+$j
+write-host $k
