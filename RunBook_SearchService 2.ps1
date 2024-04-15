fec58b740cc63541dce5d68493dbeabb1723570d
narinani225 <100519638+narinani225@users.noreply.github.com>
2024-04-16 02:24:32 +0530

    Update RunBook_SearchService 2.ps1

--- RunBook_SearchService 2.ps1
+++ RunBook_SearchService 2.ps1
@@ -1,5 +1,7 @@
 # Connect to Azure account (ensure you're logged in)
- Connect-AzAccount -Tenant '' -SubscriptionId ''
+ #Connect-AzAccount -Tenant 'e1c5d9ee-a951-451e-8432-642c23d40071' -SubscriptionId '1d012c12-eb7c-4bc4-aba7-d34e5c20bf00'
+ az login --tenant "e1c5d9ee-a951-451e-8432-642c23d40071"
+  az account set --subscription "R and D Labs"
  # Set the name of your App Service, storage account, and file share
 $appServiceName = "rg-workspace-ui-app"
 $storageAccountName = "rgworkspacesg"
@@ -14,19 +16,6 @@ $appServicePlanName = "rg-workspace-serviceplan"
 $storageAccountKey = "a5f+AK/Q974cBVEW7uRSR6zTLgo8HvkbvMLTtHZifL42M7TglsHLjN2dUhUR4q4F7cz8wzjmxK5r+AStKwmZAA=="
 # Add the storage account as a mount
 
-
-# Construct the storage account properties
-$storageAccountProperties = @{
-    "CustomId" = $storageAccountName
-    "StorageType" = "AzureFiles"
-    "ShareName" = $fileShareName
-    "AccountName" = $storageAccountName
-    "AccessKey" = $storageAccountKey
-    "MountPath" = $containerPath
-}
-
-# Add the storage account as a mount
-Add-AzWebAppStorageAccount -ResourceGroupName $resourceGroupName -Name $appServiceName -CustomId $storageAccountName -StorageType AzureFiles -ShareName $fileShareName -AccountName $storageAccountName -AccessKey $storageAccountKey -MountPath $containerPath
-
+az webapp config storage-account add --resource-group $resourceGroupName --name $appServiceName --custom-id $storageAccountName --storage-type AzureFiles --share-name $fileShareName --account-name $storageAccountName --access-key $storageAccountKey --mount-path $containerPath
 # Verify the storage is mounted
-Get-AzWebAppStorageAccount -ResourceGroupName $resourceGroupName -Name $appServiceName
+az webapp config storage-account list --resource-group $resourceGroupName --name $appServiceName
