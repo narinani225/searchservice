5468d71b7c3e41297821b6e1b7f02c4960eff853
narinani225 <100519638+narinani225@users.noreply.github.com>
2024-04-16 03:58:56 +0530

    Update RunBook_SearchService 2.ps1

--- RunBook_SearchService 2.ps1
+++ RunBook_SearchService 2.ps1
@@ -1,1 +1,15 @@
 write-host $pwd
+# Set the API URL
+$apiUrl = "https://rg-workspace-api-app.azurewebsites.net/api"
+
+# Create the JSON object
+$jsonObject = @{
+    "API_URL" = $apiUrl
+}
+
+# Convert the JSON object to a string
+$jsonString = $jsonObject | ConvertTo-Json
+
+# Write the JSON string to a file
+$jsonString | Out-File -FilePath "api-url.json"
+ls
